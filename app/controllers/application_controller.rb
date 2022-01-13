dcdc694302624026d19485ba740039c519219db9
EC2 Default User <ec2-user@ip-172-31-21-229.ec2.internal>
2022-01-13 09:37:57 +0000

    [Finish]4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,25 @@ class ApplicationController < ActionController::Base
 # このように記述することで、devise利用の機能（ユーザ登録、ログイン認証など）
 # が使われる前にconfigure_permitted_parametersメソッドが実行されます。
 
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  # after_sign_in_path_forはDeviseが用意しているメソッドで、
+  # サインイン後にどこに遷移するかを設定しているメソッドです。
+  # after_sign_in_path_forは、Deviseの初期設定ではroot_pathになっています。
+  # サインイン後にルートパスに遷移していたのはこのためです。
+  # 上記のような記述をすることで、初期設定を上書きすることができます。
+  # 今回はAboutページへ遷移するように設定しました。
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  # 上のsign_in_pathの逆
+
   protected
 # privateは記述をしたコントローラ内でしか参照できません。
 # 一方、protectedは呼び出された他のコントローラからも参照することができます。
-  
+
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
