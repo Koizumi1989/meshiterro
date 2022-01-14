3fdb63a4183d642dff8be9a733523caf3b4d2ebb
EC2 Default User <ec2-user@ip-172-31-21-229.ec2.internal>
2022-01-14 14:14:37 +0000

    [Finish]4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :post_images,dependent: :destroy
   # has many post image（たくさんpost imageを持っている）
   # dependent destroy(1:Nの1側が消去された時、N側全消去)
   # 1側：user N側：post image
+  
+  has_one_attached :profile_image
+  # has_one_attached :profile_imageという記述により、profile_imageという
+  # 名前でActiveStorageでプロフィール画像を保存できるように設定しました。
+  
+  def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize: size).processed
+  end
+  # 同じような記述をPostImageモデルに記述を行いました
+  # メソッドに対して引数を設定し、引数に設定した値に画像
+  # のサイズを変換するようにしました。
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :users,only[:show,:edit]
   resources :post_images,only:[:new,:create,:index,:show,:destroy]
   # get 'post_images/new'
   # get 'post_images/index'
