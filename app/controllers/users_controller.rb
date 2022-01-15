ce95e88ca6881ea35e7fd55fc6c1f4c040c5b87a
EC2 Default User <ec2-user@ip-172-31-21-229.ec2.internal>
2022-01-15 06:07:17 +0000

    [Finish]4-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+    # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）
+    # を取得し@post_imagesに渡す という処理を行うことができます。
+    # 結果的に、全体の投稿ではなく、個人が投稿したもの全てを表示できます。
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-
-  resources :users,only[:show,:edit]
-  resources :post_images,only:[:new,:create,:index,:show,:destroy]
+  devise_for :users
+  root to: 'homes#top'
+  get "homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :index, :show]
+  resources :users, only: [:show, :edit]
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
   # 今回は新規投稿、一覧、詳細機能、消去しか使わないため
   # create追加
-  
-  devise_for :users
-  root to: "homes#top"
-  get 'homes/about' => 'homes#about',as:'about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
